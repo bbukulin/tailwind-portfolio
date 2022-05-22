0637311eea1ac2ec36054056cbebbf6968171704
Borna <bukulin.career@gmail.com>
2022-05-22 21:23:04 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FC5C7D',
+        'secondary': '#6A82FB',
+      },
+    },
+  },
+  plugins: [],
+}
